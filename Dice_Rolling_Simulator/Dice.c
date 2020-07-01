1d554bf144fb47cac5d18a4aa89d01fb3503f652
Christine Tang <tang.c.christine@gmail.com>
2020-06-30 22:42:14 -0400

    Version 5
    Fixed the Dice Rolling Simulator by fixing the formula

--- Dice_Rolling_Simulator/Dice.c
+++ Dice_Rolling_Simulator/Dice.c
@@ -11,7 +11,7 @@
 int main(void)
 {
     //Using the random formula from the following link: https://www.tutorialspoint.com/c_standard_library/c_function_rand.htm#:~:text=The%20C%20library%20function%20int,to%20be%20at%20least%2032767.
-    //Formula: https://www.programmingsimplified.com/c-program-generate-random-numbers
+    //Formula: https://stackoverflow.com/questions/1202687/how-do-i-get-a-specific-range-of-numbers-from-rand
     srand(time(0)); 
 
     //Define Mix and Max for a 6-sided dice
@@ -28,10 +28,45 @@ int main(void)
     //For loop
     for (int i =0; i< count; i++)
     {
-        int nRandomNumber = rand() % (nMax + 1);
-        printf(" The diced rolled... %d \n", nRandomNumber); //taken from Stack Overflow site
+        int nR = rand() % (nMax - nMin + 1) + nMin;
+        printf(" The diced rolled... %d \n", nR); //taken from Stack Overflow site
         sleep(1);
+
+        //Tally the number of times each number was rolled, for testing purposes
+        if (nR == 1) //count the 1's
+        {
+            int count_1 = count_1+ 1;
+        }
+
+        if (nR == 2) //count the 2's
+        {
+            int count_2 = count_2+ 1;
+        }
+
+        if (nR == 3) // count the 3's
+        {
+            int count_3 = count_3+ 1;
+        }
+
+        if (nR == 4) // count the 3's
+        {
+            int count_4 = count_4+ 1;
+        }
+
+        if (nR == 5) // count the 3's
+        {
+            int count_5 = count_5+ 1;
+        }
+
+        if (nR == 6) // count the 3's
+        {
+            int count_6 = count_6+ 1;
+        }
+
     }
-    return 0;
+    
+    printf(" Printed the follwing: \n");
+    
+
 
 }
