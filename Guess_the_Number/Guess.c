f440abdd6ea18f8b3a5f41234aafb915046bef6d
Christine Tang <tang.c.christine@gmail.com>
2020-07-02 22:19:46 -0400

    Update
    Added more functions, about 30% done I'd say.

--- Guess_the_Number/Guess.c
+++ Guess_the_Number/Guess.c
@@ -8,7 +8,9 @@
 #include <time.h> 
 #include <unistd.h>
 
-//int guess(int a, int b)
+//Functions used:
+int guess(a,b)
+
 
 int main (void)
 {
@@ -22,12 +24,14 @@ int main (void)
 
     // Generate a number first
     int nAnswer = rand() % (nMax - nMin + 1) + nMin;
-    printf("%i \n", nAnswer);
 
     //Ask for a guess
     printf("Guess the number: ");
-    scanf("%i", nGuess);
-    printf("Your guess was: %i \n", nGuess);
+    scanf("%d", &nGuess);
+
+    //Evaluate the Guess
+    guess(&nGuess, &nAnswer);
+    
 
 
     return 0;
@@ -36,20 +40,21 @@ int main (void)
 
 
 //Function to GUESS the answer.
-// int guess(int a, int b)
-// {
-//     if (a == b)
-//     {
-//         printf("Congrats! You got the number right! Let's play again :) \n");
-//     }
+int guess(int a, int b)
+{
+    if (a == b)
+    {
+        printf("Congrats! You got the number right! Let's play again :) \n");
+    }
 
-//     else
-//     {
-//         //Game needs to continue
-//         return 1;
-//     }
-//     return 0;
-// }
+    else
+    {
+        //Game needs to continue
+        return 1; //true
+    }
+    return 0; //false
+
+}
 
 
 //Function to Answer the question: ODD or EVEN #
