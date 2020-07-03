907640b6a8e2f797a2662cd1d9386a8cac3e7e1f
Christine Tang <tang.c.christine@gmail.com>
2020-07-03 17:17:44 -0400

    Update
    CODE WORKS! It evaluates right/wrong, repeats asking for new guess until you get it right.
    
    Will implement questions functions in a bit.

--- Guess_the_Number/Guess.c
+++ Guess_the_Number/Guess.c
@@ -9,9 +9,13 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-//Functions used:
-bool guess(int *a, int *b);
+//Global Variables
 
+
+//Functions used:
+bool evaluate(int *a, int *b); //Guessing function
+void oddeven(int *a); //Odd-even function
+void guess(int *a);
 
 int main(void)
 {
@@ -22,41 +26,70 @@ int main(void)
 
     //Initialize other variables:
     int nGuess = 0;
+    int nAnswer = 0;
 
     // Generate a number first
-    int nAnswer = rand() % (nMax - nMin + 1) + nMin;
+    nAnswer = rand() % (nMax - nMin + 1) + nMin;
     printf("Pst! The right number is %i \n", nAnswer);
 
-    //Ask for a guess
-    printf("Guess the number: ");
-    scanf("%d", &nGuess);
+    //Begin loop - ask and guess
 
-    //Evaluate the Guess
-    guess(&nGuess, &nAnswer);
+    while (true)
+    {
+        guess(&nGuess);
+        printf("Your guess is: %i \n", nGuess);
+        bool TF = evaluate(&nGuess, &nAnswer); 
+        if (TF)
+        {
+            break;
+        }
+    }
+
     
-
-
-    return 0;
 
 }
 
+//Function to ask for a NEW guess (assuming they didn't get it the first time)
+void guess(int *a)
+{
+    printf("Guess the Number: ");
+    scanf("%d", a);
+    return;
+}
 
 //Function to GUESS the answer.
-bool guess(int *a, int *b)
+bool evaluate(int *a, int *b)
 {
     if (*a == *b)
     {
-        printf("Congrats! You got the number right! Let's play again :) \n");
+        printf("Congrats! You got the number right! Let's play again! :) \n");
         return 1; //true
     }
-    return 0; //false
-
+    return 0; // false
 }
 
-
 //Function to Answer the question: ODD or EVEN #
+void oddeven(int *a)
+{
+    if ((*a % 2) == 0)
+    {
+        //The number is even!
+        printf("Yes, the number is even. \n There's your hint! \n");
+    }
+    if ((*a % 2) != 0)
+    {
+        printf("Your number is odd. \n There's your hint! \n");
+    }
+    
+    return;
+}
 
 //Function to Answer the question: Is this divisible by X?
 
 //Function to Quit This Game
+void quit(char *s)
+{
 
+
+
+}
