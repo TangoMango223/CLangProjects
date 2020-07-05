3f21be0031792f83164fbac05ccbf6e025ad4b21
Christine Tang <tang.c.christine@gmail.com>
2020-07-05 15:21:54 -0400

    Update
    Starting Hash table code

---
+++ Data-Structures/hashtable.c
@@ -0,0 +1,13 @@
+// Code is practicing implementing a hash table in C
+// Link for reference: https://www.youtube.com/watch?v=2Ti5yvumFTU
+
+#include <ctype.h>
+#include <math.h>
+#include <string.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+#include <unistd.h>
+#include <stdbool.h>
+
+
