26bb30ec102f06452cbe4bb58bd75830224bc6b9
Christine Tang <tang.c.christine@gmail.com>
2020-07-05 15:26:23 -0400

    Update

--- Data-Structures/hashtable.c
+++ Data-Structures/hashtable.c
@@ -10,4 +10,32 @@
 #include <unistd.h>
 #include <stdbool.h>
 
+#define MAX_NAME 256
+#define TABLE_SIZE 10
+
+//Define data structure to hold the person's info
+typedef struct{
+    char name[MAX_NAME];
+    int age;
+    // ...other stuff later
+} person; //call this data structure PERSON
+
+//Hashfunction - look up people by name:
+unsigned int hash(char *name)
+{
+    return 5;
+}
+
+
+int main (void)
+{
+    //Example different names
+    printf("Jacob => %u \n", hash("Jacob"));
+    printf("Natalie => %u \n", hash("Natalie"));
+    printf("Sara => %u \n", hash("Sara"));
+    printf("Mpho => %u \n", hash("Mpho"));
+    printf("Tebogo => %u \n", hash("Tebogo"));
+    return 0;
+
+}
 
